6376a2ba58956a0c09deb1fe8fa1ff3673f05880
Frank Maranje <frank.maranje@sti-tec.com>
2023-07-14 16:14:24 -0400

    Initial Commit

---
+++ RADc/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RADc
+//
+//  Created by Frank Maranje on 7/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RADc/RADcApp.swift
@@ -0,0 +1,17 @@
+//
+//  RADcApp.swift
+//  RADc
+//
+//  Created by Frank Maranje on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RADcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
