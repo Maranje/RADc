417cf7a88d42e018a961d7665e6048384a1c3f36
Frank Maranje <frank.maranje@sti-tec.com>
2023-07-24 15:07:43 -0400

    add bounce effect and user prompt to splash screen

--- RADc/SplashPage.swift
+++ RADc/SplashPage.swift
@@ -11,24 +11,59 @@ struct SplashPage: View {
     
     //MARK: properties
     @State var splash: Bool = true
+    @State var bounce: Bool = false
+    @State var tapPrompt: Bool = false
     
     //MARK: splash page body
     var body: some View {
         ZStack{
             
             if splash{
-                
-                //create app splash page
-                Image("RADc")
-                    .resizable()
-                    .frame(width: 300, height: 300)
-                    .cornerRadius(50)
-                    .onTapGesture {
-                        
-                        //set splash to false and load AnthroForm
-                        withAnimation{ splash = false }
-                        
+                VStack{
+                    //create app splash page
+                    Image("RADc")
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                        .cornerRadius(50)
+                        .onTapGesture {
+                            
+                            //set splash to false and load AnthroForm
+                            withAnimation{ splash = false }
+                            
+                        }
+                        .scaleEffect(bounce ? 1.1 : 1.0)
+                        .onAppear {
+                            // Start the bounce timer when the view appears
+                            Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+                                withAnimation(.easeInOut(duration: 0.2)) {
+                                    // Toggle the isBouncing state variable to trigger the animation
+                                    bounce = true
+                                    //run after 0.25 seconds
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+
+                                        //set bounce back to false within "withAnimation" to animate scale decrease effect
+                                        withAnimation{ bounce = false}
+
+                                    }
+                                }
+                            }
+
+                            //start the prompt timer when the view appears, only run once
+                            Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false){ _ in
+                                withAnimation{
+                                    //set tapPrompt to true and show the text
+                                    tapPrompt = true
+                                }
+                            }
+                        }
+                    
+                    if tapPrompt{
+                        //prompt text
+                        Text("Tap Logo To Begin").fontWeight(.thin).foregroundColor(.gray)
                     }
+                    
+                }
+        
             }
             else{
                 
