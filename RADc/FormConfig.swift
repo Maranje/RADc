4c900e316e2ac7814cde1ec8bd86bf85c46eda0f
Frank Maranje <frank.maranje@sti-tec.com>
2023-07-24 15:18:47 -0400

    add "deselect/reselect" button to form configuration

--- RADc/FormConfig.swift
+++ RADc/FormConfig.swift
@@ -16,6 +16,7 @@ struct FormConfig: View{
     @Binding var labels: [String]
     @Binding var labelsStanding: [String]
     @Binding var labelsSitting: [String]
+    @State var selectAll: Bool = true
     
     //MARK: form config body
     var body: some View{
@@ -83,66 +84,81 @@ struct FormConfig: View{
                 
                 Divider()
                     .padding(/*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
-                
-                //"create form" button to create the form and load all the chosen measurement labels into the appropriate string arrays
-                Button("Create Form"){
-                    if(measurements[1]){labels.append("Name")}
-                    if(measurements[2]){labels.append("Phone Number")}
-                    if(measurements[3]){labels.append("Email")}
-                    if(measurements[4]){labels.append("Age")}
-                    if(measurements[5]){labels.append("Aircraft")}
-                    if(measurements[6]){labels.append("Weight")}
-                    if(measurements[7]){labels.append("AFSC")}
-                    if(measurements[8]){labels.append("Gender")}
-                    if(measurements[9]){labels.append("Ethnicity")}
-                    if(measurements[10]){labelsStanding.append("Stature, standing")}
-                    if(measurements[11]){labelsSitting.append("Stature, sitting")}
-                    if(measurements[12]){labelsStanding.append("Eye Height, standing")}
-                    if(measurements[13]){labelsSitting.append("Eye Height, sitting")}
-                    if(measurements[14]){labelsStanding.append("Acromial Height, standing")}
-                    if(measurements[15]){labelsSitting.append("Acromial Height, sitting")}
-                    if(measurements[16]){labelsStanding.append("Suprasternale Height")}
-                    if(measurements[17]){labelsStanding.append("Waist Height, omphalion")}
-                    if(measurements[18]){labelsStanding.append("Waist Front Length")}
-                    if(measurements[19]){labelsStanding.append("Chest Circumference @ Scye")}
-                    if(measurements[20]){labelsStanding.append("Chest Circumference")}
-                    if(measurements[21]){labelsStanding.append("Waist Circumference, omphalion")}
-                    if(measurements[22]){labelsStanding.append("Waist Circumference, natural")}
-                    if(measurements[23]){labelsStanding.append("Max Hip Circumference")}
-                    if(measurements[24]){labelsStanding.append("Thigh Circumference")}
-                    if(measurements[25]){labelsStanding.append("Vertical Trunk Circumference")}
-                    if(measurements[26]){labelsStanding.append("Kneeling Reach")}
-                    if(measurements[27]){labelsStanding.append("Arm Span")}
-                    if(measurements[28]){labelsStanding.append("Thumb Tip Reach, average")}
-                    if(measurements[29]){labelsStanding.append("Overhead Reach, flat, shoes off")}
-                    if(measurements[30]){labelsStanding.append("Overhead Reach, tiptoes, shoes off")}
-                    if(measurements[31]){labelsStanding.append("Overhead Reach, flat, shoes/uniform on")}
-                    if(measurements[32]){labelsStanding.append("Overhead Reach, tiptoes, shoes/uniform on")}
-                    if(measurements[33]){labelsSitting.append("Overhead Reach, sitting")}
-                    if(measurements[34]){labelsSitting.append("Knee Height, sitting")}
-                    if(measurements[35]){labelsSitting.append("Thigh Clearance, sitting")}
-                    if(measurements[36]){labelsSitting.append("Bideltoid Breadth")}
-                    if(measurements[37]){labelsSitting.append("Biacromial Breadth")}
-                    if(measurements[38]){labelsSitting.append("Hip Breadth, sitting")}
-                    if(measurements[39]){labelsSitting.append("Abdomianl Depth")}
-                    if(measurements[40]){labelsSitting.append("Buttock-Knee Length")}
-                    if(measurements[41]){labelsSitting.append("Finger Tip, right")}
-                    if(measurements[42]){labelsSitting.append("Finger Tip, left")}
-                    if(measurements[43]){labelsSitting.append("Hook, right")}
-                    if(measurements[44]){labelsSitting.append("Hook, left")}
-                    if(measurements[45]){labelsSitting.append("Thumb Tip, right")}
-                    if(measurements[46]){labelsSitting.append("Thumb Tip, left")}
-                    if(measurements[47]){labelsSitting.append("Grip, right")}
-                    if(measurements[48]){labelsSitting.append("Grip, left")}
+                HStack{
+                    //"create form" button to create the form and load all the chosen measurement labels into the appropriate string arrays
+                    Button("Create Form"){
+                        if(measurements[1]){labels.append("Name")}
+                        if(measurements[2]){labels.append("Phone Number")}
+                        if(measurements[3]){labels.append("Email")}
+                        if(measurements[4]){labels.append("Age")}
+                        if(measurements[5]){labels.append("Aircraft")}
+                        if(measurements[6]){labels.append("Weight")}
+                        if(measurements[7]){labels.append("AFSC")}
+                        if(measurements[8]){labels.append("Gender")}
+                        if(measurements[9]){labels.append("Ethnicity")}
+                        if(measurements[10]){labelsStanding.append("Stature, standing")}
+                        if(measurements[11]){labelsSitting.append("Stature, sitting")}
+                        if(measurements[12]){labelsStanding.append("Eye Height, standing")}
+                        if(measurements[13]){labelsSitting.append("Eye Height, sitting")}
+                        if(measurements[14]){labelsStanding.append("Acromial Height, standing")}
+                        if(measurements[15]){labelsSitting.append("Acromial Height, sitting")}
+                        if(measurements[16]){labelsStanding.append("Suprasternale Height")}
+                        if(measurements[17]){labelsStanding.append("Waist Height, omphalion")}
+                        if(measurements[18]){labelsStanding.append("Waist Front Length")}
+                        if(measurements[19]){labelsStanding.append("Chest Circumference @ Scye")}
+                        if(measurements[20]){labelsStanding.append("Chest Circumference")}
+                        if(measurements[21]){labelsStanding.append("Waist Circumference, omphalion")}
+                        if(measurements[22]){labelsStanding.append("Waist Circumference, natural")}
+                        if(measurements[23]){labelsStanding.append("Max Hip Circumference")}
+                        if(measurements[24]){labelsStanding.append("Thigh Circumference")}
+                        if(measurements[25]){labelsStanding.append("Vertical Trunk Circumference")}
+                        if(measurements[26]){labelsStanding.append("Kneeling Reach")}
+                        if(measurements[27]){labelsStanding.append("Arm Span")}
+                        if(measurements[28]){labelsStanding.append("Thumb Tip Reach, average")}
+                        if(measurements[29]){labelsStanding.append("Overhead Reach, flat, shoes off")}
+                        if(measurements[30]){labelsStanding.append("Overhead Reach, tiptoes, shoes off")}
+                        if(measurements[31]){labelsStanding.append("Overhead Reach, flat, shoes/uniform on")}
+                        if(measurements[32]){labelsStanding.append("Overhead Reach, tiptoes, shoes/uniform on")}
+                        if(measurements[33]){labelsSitting.append("Overhead Reach, sitting")}
+                        if(measurements[34]){labelsSitting.append("Knee Height, sitting")}
+                        if(measurements[35]){labelsSitting.append("Thigh Clearance, sitting")}
+                        if(measurements[36]){labelsSitting.append("Bideltoid Breadth")}
+                        if(measurements[37]){labelsSitting.append("Biacromial Breadth")}
+                        if(measurements[38]){labelsSitting.append("Hip Breadth, sitting")}
+                        if(measurements[39]){labelsSitting.append("Abdomianl Depth")}
+                        if(measurements[40]){labelsSitting.append("Buttock-Knee Length")}
+                        if(measurements[41]){labelsSitting.append("Finger Tip, right")}
+                        if(measurements[42]){labelsSitting.append("Finger Tip, left")}
+                        if(measurements[43]){labelsSitting.append("Hook, right")}
+                        if(measurements[44]){labelsSitting.append("Hook, left")}
+                        if(measurements[45]){labelsSitting.append("Thumb Tip, right")}
+                        if(measurements[46]){labelsSitting.append("Thumb Tip, left")}
+                        if(measurements[47]){labelsSitting.append("Grip, right")}
+                        if(measurements[48]){labelsSitting.append("Grip, left")}
+                        
+                        withAnimation{
+                            
+                            //set newForm binding to false, return to "Form"
+                            newForm = false
+                            
+                        }
+                        
+                    }.padding().background(.green).foregroundColor(.white).cornerRadius(10)//button visibility properties
                     
-                    withAnimation{
-                        
-                        //set newForm binding to false, return to "Form"
-                        newForm = false
-                        
+                    //buttons for toggling all values selected or de-selected
+                    if selectAll{
+                        Button("Deselect All"){
+                            measurements = Array(repeating: false, count: 49)
+                            selectAll = false
+                        }.padding().background(.gray).foregroundColor(.white).cornerRadius(10)//button visibility properties
                     }
-                    
-                }.padding().background(.green).foregroundColor(.white).cornerRadius(10)//button visibility properties
+                    else{
+                        Button("Reselect All"){
+                            measurements = Array(repeating: true, count: 49)
+                            selectAll = true
+                        }.padding().background(.gray).foregroundColor(.white).cornerRadius(10)//button visibility properties
+                    }
+                }
             }
         }.padding()
     }
