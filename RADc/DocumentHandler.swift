5abb15bfe1c853730853480c7177c617a4b3dfdd
Frank Maranje <frank.maranje@sti-tec.com>
2023-07-25 14:03:00 -0400

    add DocumentHandler and Info property list

---
+++ RADc/DocumentHandler.swift
@@ -0,0 +1,39 @@
+//
+//  DocumentHandler.swift
+//  RADc
+//
+//  Created by Frank Maranje on 7/25/23.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+extension UTType {
+    static var exampleText: UTType {
+        UTType(importedAs: "com.microsoft.excel.xls")
+    }
+}
+
+struct testDocument: FileDocument {
+    var text: String
+
+    init(text: String = "") {
+        self.text = text
+    }
+
+    static var readableContentTypes: [UTType] { [.exampleText] }
+
+    init(configuration: ReadConfiguration) throws {
+        guard let data = configuration.file.regularFileContents,
+              let string = String(data: data, encoding: .utf8)
+        else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+        text = string
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let data = text.data(using: .utf8)!
+        return .init(regularFileWithContents: data)
+    }
+}
