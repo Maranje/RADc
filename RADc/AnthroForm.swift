73ecf8c61880af5597fd396840e825d80033bb5c
Frank Maranje <frank.maranje@sti-tec.com>
2023-08-02 14:57:25 -0400

    fix form configuration select/deselect all bug. add weight to selected item font in participant list

--- RADc/AnthroForm.swift
+++ RADc/AnthroForm.swift
@@ -159,7 +159,7 @@ struct AnthroForm: View {
                                 Button("ID: \(participant.properties["Participant ID"] ?? "")\n\(participant.properties["Name"] ?? "")"){
                                     currentParticipant = participants[participant.pNum - 1] //load selected participant from list
                                     loadedParticipant = true
-                                }
+                                }.fontWeight((currentParticipant.pNum == participant.pNum) ? .bold : .light)
                                 
                             }
                             .frame(width:200)
